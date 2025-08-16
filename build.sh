ab853953299f30b3be77de2182ae71cde51ac8dc
BarbaraVera <barbara.alejandra.v3@gmail.com>
2025-08-16 17:07:53 -0400

    cambio en build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,11 @@
 set -o errexit
 
 pip install -r requirements.txt
+# 2. Instalar las dependencias de Node.js (incluyendo Tailwind)
+npm install
+
+# 3. Compilar el CSS de Tailwind para producción
+npm run build
 
 python manage.py collectstatic --no-input
 python manage.py migrate
